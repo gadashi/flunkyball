589f73868d29f0727023ce83e953f88829e13aaa
gadashi <42239453+gadashi@users.noreply.github.com>
2018-08-10 00:52:44 +0200

    Update test.php

--- test.php
+++ test.php
@@ -3,7 +3,7 @@
 </head>
 <body>
 <?php
-$myfile = fopen("empty.txt", "w") or die("Unable to open file!");
+$myfile = fopen("test.txt", "w") or die("Unable to open file!");
 $txt = "John Doe\n";
 fwrite($myfile, $txt);
 $txt = "Jane Doe\n";
