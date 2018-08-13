344de5d07c133f489249c33fbb0ffefc9335f678
gadashi <42239453+gadashi@users.noreply.github.com>
2018-08-13 17:14:07 +0200

    Add files via upload

---
+++ hostGame/database.js
@@ -0,0 +1,102 @@
+//CLient ID:21449399299-k4ln8srecgb3u0ejn3vdt80k3eeq1jv6.apps.googleusercontent.com
+//Client secret:1bYKj5bZZtcInTejJgB3ujDU
+//API key: AIzaSyAh2TO5vxJchkAKTL_dyIR7yOmfzrNpC5k
+//Table Id: 1QC0B1p0LdTS2vE8l-bE8zueFOiBqUctHLeWZSzxWFb4
+
+function read(source) {
+  var params = {
+    spreadsheetId: '1QC0B1p0LdTS2vE8l-bE8zueFOiBqUctHLeWZSzxWFb4',
+    range: source,
+    //valueRenderOption: '',
+    //dateTimeRenderOption: '',
+  };
+
+  var request = gapi.client.sheets.spreadsheets.values.get(params);
+  request.then(function(response) {
+    // TODO: Change code below to process the `response` object:
+    console.log(response.result);
+    if(source=="Tabellenblatt1"){
+
+    }
+    else if(source=="Spiele"){
+
+    }
+  }, function(reason) {
+    console.error('error: ' + reason.result.error.message);
+  });
+}
+
+function write(){
+  var params = {
+    // The ID of the spreadsheet to update.
+    spreadsheetId: '1QC0B1p0LdTS2vE8l-bE8zueFOiBqUctHLeWZSzxWFb4',  // TODO: Update placeholder value.
+    // The A1 notation of the values to update.
+    range: 'Tabellenblatt1',  // TODO: Update placeholder value.
+    // How the input data should be interpreted.
+    valueInputOption: 'RAW',  // TODO: Update placeholder value.
+  };
+
+  var valueRangeBody = {
+    // TODO: Add desired properties to the request body. All existing properties
+    // will be replaced.
+  };
+
+  var request = gapi.client.sheets.spreadsheets.values.update(params, valueRangeBody);
+  request.then(function(response) {
+    // TODO: Change code below to process the `response` object:
+    console.log(response.result);
+  }, function(reason) {
+    console.error('error: ' + reason.result.error.message);
+  });
+}
+
+function initClient() {
+  var API_KEY = 'AIzaSyAh2TO5vxJchkAKTL_dyIR7yOmfzrNpC5k';
+
+  var CLIENT_ID = '21449399299-k4ln8srecgb3u0ejn3vdt80k3eeq1jv6.apps.googleusercontent.com';
+  //   'https://www.googleapis.com/auth/spreadsheets'
+  var SCOPE = 'https://www.googleapis.com/auth/spreadsheets';
+
+  gapi.client.init({
+    'apiKey': API_KEY,
+    'clientId': CLIENT_ID,
+    'scope': SCOPE,
+    'discoveryDocs': ['https://sheets.googleapis.com/$discovery/rest?version=v4'],
+  }).then(function() {
+    gapi.auth2.getAuthInstance().isSignedIn.listen(updateSignInStatus);
+    updateSignInStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
+  });
+}
+
+function handleClientLoad() {
+  gapi.load('client:auth2', initClient);
+}
+
+function updateSignInStatus(isSignedIn) {
+  if (isSignedIn) {
+    read("Tabellenblatt1"); //TODO: change Tabellenblatt1 to Spieler in here, getPlayerStats and database
+    read("Spiele");
+  }
+}
+
+function handleSignInClick() {
+  gapi.auth2.getAuthInstance().signIn();
+}
+
+function handleSignOutClick() {
+  gapi.auth2.getAuthInstance().signOut();
+}
+
+function players(result){
+
+}
+
+function activeGames(result){
+
+}
+
+
+
+window.onload = function(){
+  handleSignInClick();
+}
