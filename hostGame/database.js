0318c7113c55f4ba1ddc89f33d0923cc2bb65848
gadashi <42239453+gadashi@users.noreply.github.com>
2018-08-15 19:14:14 +0200

    Update database.js

--- hostGame/database.js
+++ hostGame/database.js
@@ -32,26 +32,28 @@ function read(source) {
 
 function write(){
   var params = {
-    // The ID of the spreadsheet to update.
-    spreadsheetId: '1QC0B1p0LdTS2vE8l-bE8zueFOiBqUctHLeWZSzxWFb4',  // TODO: Update placeholder value.
-    // The A1 notation of the values to update.
-    range: 'Spiele',  // TODO: Update placeholder value.
-    // How the input data should be interpreted.
-    valueInputOption: 'RAW',  // TODO: Update placeholder value.
-  };
+          // The ID of the spreadsheet to update.
+          spreadsheetId: '1QC0B1p0LdTS2vE8l-bE8zueFOiBqUctHLeWZSzxWFb4',  // TODO: Update placeholder value.
+        };
 
-   var valueRangeBody = {
+        var batchUpdateValuesRequestBody = {
+          valueInputOption: 'RAW',
+          "data": [
+                    {
+                      "range": "Spiele!A2:B3",
+                      "values": [[1,2],[3,4]]
+                    }
+                  ],
+                };
 
-    values: [[1,2],[3,4]],
-  };
+        var request = gapi.client.sheets.spreadsheets.values.batchUpdate(params, batchUpdateValuesRequestBody);
+        request.then(function(response) {
+          // TODO: Change code below to process the `response` object:
+          console.log(response.result);
+        }, function(reason) {
+          console.error('error: ' + reason.result.error.message);
+        });
 
-  var request = gapi.client.sheets.spreadsheets.values.update(params, valueRangeBody);
-  request.then(function(response) {
-    // TODO: Change code below to process the `response` object:
-    console.log(response.result);
-  }, function(reason) {
-    console.error('error: ' + reason.result.error.message);
-  });
 }
 
 function initClient() {
