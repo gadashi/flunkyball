15237868a7c9812fe01c432679cfd1e8d73fb8ec
gadashi <42239453+gadashi@users.noreply.github.com>
2018-08-17 00:26:35 +0200

    Update scriptSpiel.js

--- hostGame/scriptSpiel.js
+++ hostGame/scriptSpiel.js
@@ -164,6 +164,7 @@ finishedPlayers.push([id,numberOfHits]);
   var updateCellValue = "";
   for(var i=0; i<finishedPlayers.length;i++){
     updateCellValue += finishedPlayers[i][0]+ ",";
+    console.log(finishedPlayers[i][0],updateCellValue);
   }
   UpdateCell(row+ 1,col,0);
   console.log(finishedPlayers);
