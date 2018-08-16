ae61545052653f22e42ac2c3a46bf203fde6de02
gadashi <42239453+gadashi@users.noreply.github.com>
2018-08-16 17:21:52 +0200

    Update scriptSpiel.js

--- hostGame/scriptSpiel.js
+++ hostGame/scriptSpiel.js
@@ -7,7 +7,7 @@
 function read(gameID) {
   var params = {
     spreadsheetId: '1QC0B1p0LdTS2vE8l-bE8zueFOiBqUctHLeWZSzxWFb4',
-    range: 'aktiveSpiele!A' + (gameID * 4 +1) + ':L'(gameID * 4 +4),
+    range: 'aktiveSpiele!A' + (gameID * 4 +1) + ':L'+(gameID * 4 +4),
     //valueRenderOption: '',
     //dateTimeRenderOption: '',
   };
