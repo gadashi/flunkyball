5e2d02fbe66539294669783ebd753c02e6b2bfab
gadashi <42239453+gadashi@users.noreply.github.com>
2018-08-16 22:06:00 +0200

    Update scriptSpiel.js

--- hostGame/scriptSpiel.js
+++ hostGame/scriptSpiel.js
@@ -50,6 +50,33 @@ function writeData(data){
 
 }
 
+function UpdateCell(rows,cols,data) {
+  console.log(cols + rows);
+   var params = {
+     // The ID of the spreadsheet to update.
+     spreadsheetId: '1QC0B1p0LdTS2vE8l-bE8zueFOiBqUctHLeWZSzxWFb4',  // TODO: Update placeholder value.
+
+     // The A1 notation of the values to update.
+     range: 'aktiveSpiele!' + cols + rows +":"+ cols + rows ,  // TODO: Update placeholder value.
+    
+     // How the input data should be interpreted.
+     valueInputOption: 'RAW',  // TODO: Update placeholder value.
+   };
+
+   var valueRangeBody = {
+     'values': data;// TODO: Add desired properties to the request body. All existing properties
+     // will be replaced.
+   };
+
+   var request = gapi.client.sheets.spreadsheets.values.update(params, valueRangeBody);
+   request.then(function(response) {
+     // TODO: Change code below to process the `response` object:
+     console.log(response.result);
+   }, function(reason) {
+     console.error('error: ' + reason.result.error.message);
+   });
+ }
+
 function initClient() {
   var API_KEY = 'AIzaSyAh2TO5vxJchkAKTL_dyIR7yOmfzrNpC5k';
 
@@ -100,8 +127,27 @@ function populateNames(Names){
       document.getElementById('Spieler' + i).innerHTML = Names.values[3][i];
     }
   }
+  row = gameID * 4 + 1;
+  numberOfThrows = Names.values[0].length;
 }
 
+var row =0;
+var numberOfThrows = 1;
+var Alphabet = ["A","B","C","D","E","F","G","H","I","J","K","L",
+                "M","N","O","P","Q","R","S","T","U","V","W","X",
+                "Y","Z"];
+function miss(id){
+var col="";
+  while(numberOfThrows >= 0){
+    col += Alphabet[numberOfThrows%26];
+    numberOfThrows -= 26;
+  }
+  console.log(row,col,id);
+  UpdateCell(row,col,id);
+  UpdateCell(row + 1,col,0);
+  UpdateCell(row +2,col,0);
+
+}
 window.onload = function(){
   handleSignInClick();
 }
